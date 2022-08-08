6e9e3fbeb52f99a0ad831562dd13878b87fb0d3f
HCoder <86558426+HCoderHY@users.noreply.github.com>
2022-08-08 11:35:05 +0300

    Update main.py

--- ML/main.py
+++ ML/main.py
@@ -7,29 +7,13 @@ from sklearn import metrics
 from sklearn.feature_extraction import image
 import tensorflow as tf
 
-# mnist = tf.keras.datasets.mnist
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-# x_train = tf.keras.utils.normalize(x_train, axis=1)
-# x_test = tf.keras.utils.normalize(x_test, axis=1)
-
-# model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(128, activation='relu'))
-# model.add(tf.keras.layers.Dense(10, activation='softmax'))
-
-# model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# model.fit(x_train, y_train, epochs=3)
-
-# model.save('handwritten.model')
-
 model = tf.keras.models.load_model('handwritten.model')
 
 image_number = 1
+# digits is a folder with images of numbers, add images to this folder
 while os.path.isfile(f"digits/{image_number}.png"):
     try:
+        # name the images like 1.png, 2.png and so on
         img = cv2.imread(f"digits/{image_number}.png")[:,:,0]
         img = np.invert(np.array([img]))
         prediction = model.predict(img)
